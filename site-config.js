09fa397a8d860e143f821b13d584fd98fa8c43b7
my-business-page <madboyzzclub@gmail.com>
2026-08-22 01:09:50 +0530

    Add central site configuration

---
+++ site-config.js
@@ -0,0 +1,1 @@
+export const site={name:'ZAARA',tagline:'Modern Indian dining',pages:[['Home','index.html'],['Menu','menu.html'],['Experience','experience.html'],['Our Story','about.html'],['Gallery','gallery.html'],['Events','events.html'],['Reserve','reserve.html'],['Visit','contact.html']]};
